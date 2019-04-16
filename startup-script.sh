7c965955a868a3226c6d5f1260980114bf0360e4
Denis Saykin <saykind@gmail.com>
2019-04-16 11:34:07 +0300

    startup script mongodb install fix

--- startup-script.sh
+++ startup-script.sh
@@ -5,7 +5,7 @@ apt install -y ruby-full ruby-bundler build-essential
 
 #mongodb install
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
